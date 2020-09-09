d85ceabdf0a273ac57a53baaaed68d272e275f04
yahoobb <rs.knuth@ezweb.ne.jp>
2020-09-09 19:34:19 +0900

    del key

--- keys.py
+++
@@ -1,4 +0,0 @@
-consumer_key = 'dNe2D9pl9MHJGSFWxt3LMx3gU'
-consumer_secret = 'IkBKfoTKt3NerzliitRiKdHsSXJYgoVptSyvxl6PLN8fuyhQiJ'
-access_token = '1303338261675307009-9pm2hiS7ha1mjgizQnvx8p1yov8oU9'
-access_token_secret = 'j9VxytD7bnN7IWoVuNULTqrzh1E1kKDacN4aoPSoyCPmb'
